9687475522e467dce9776a5940349a9ba67b72d5
Danila Fomin <dangdancheg@gmail.com>
2022-08-18 20:14:11 +0300

    Append full request error to err output Fixes #101

--- watcher.js
+++ watcher.js
@@ -1,3 +1,4 @@
+const { time } = require("console");
 const fs = require("fs");
 const execSync = require("child_process").execSync;
 const path = require("path");
@@ -84,11 +85,15 @@ var rebuild = function () {
 let count = 1;
 rebuild();
 console.log("\x1b[34m%s\x1b[0m", "Webview watcher started: " + count);
+let lastrebuild = performance.now();
 fs.watch("webview/src", function (event, filename) {
   count += 1;
   try {
-    rebuild();
-    console.log("\x1b[32m%s\x1b[0m", "Rebuilt webview for vscode: " + count);
+    if (performance.now() - lastrebuild > 4000) {
+      rebuild();
+      console.log("\x1b[32m%s\x1b[0m", "Rebuilt webview for vscode: " + count);
+      lastrebuild = performance.now();
+    }
   } catch (e) {
     console.log("\x1b[31m%s\x1b[0m", "Error rebuilding webview: " + e);
   }
