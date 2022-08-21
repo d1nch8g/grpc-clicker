61494e2a379532ecb5813bc68cdfde87e03e9f6e
Danila Fomin <dangdancheg@gmail.com>
2022-08-21 12:21:03 +0300

    .

--- src/storage/storage.ts
+++ src/storage/storage.ts
@@ -4,13 +4,15 @@ import { History } from "./history";
 import { ProtoServers } from "./protoServer";
 import { ProtoFiles } from "./protoFiles";
 import { Collections } from "./collections";
+import { Hosts } from "./hosts";
 
 export class Storage {
-  public readonly files: ProtoFiles;
-  public readonly servers: ProtoServers;
+  public readonly collections: Collections;
   public readonly headers: Headers;
   public readonly history: History;
-  public readonly collections: Collections;
+  public readonly hosts: Hosts;
+  public readonly files: ProtoFiles;
+  public readonly servers: ProtoServers;
 
   constructor(private memento: Memento) {
     if (memento.get(`grpc-clicker-version`) !== "0.1.1") {
@@ -24,6 +26,7 @@ export class Storage {
     this.headers = new Headers(memento);
     this.history = new History(memento);
     this.collections = new Collections(memento);
+    this.hosts = new Hosts(memento);
   }
 
   clean() {
