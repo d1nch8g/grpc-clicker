f19e4746de8053936ea77b41ac9e3a074cf68788
dancheg <dangdancheg@gmail.com>
2022-11-05 12:29:02 +0300

    added required files for second webview and process of it's preparation to webview watcher

---
+++ webviews/source/src/Request.svelte
@@ -0,0 +1,29 @@
+<script>
+  import JsonArea from "./JsonArea.svelte";
+  export let height;
+  export let data;
+</script>
+
+<table>
+  <tr>
+    <center>
+      <div>Request type: {data.info.inputMessageName}</div>
+    </center>
+  </tr>
+  <tr>
+    <JsonArea bind:text="{data.request.content}" height="{height}" />
+  </tr>
+</table>
+
+<style>
+  table {
+    width: 100%;
+  }
+  tr {
+    width: 100%;
+  }
+  center {
+    padding-top: 2px;
+    padding-bottom: 5px;
+  }
+</style>
